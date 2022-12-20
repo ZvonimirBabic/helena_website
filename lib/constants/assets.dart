4e4963dd7369c10e0894ca43e2ae1cb2502a24ba
BlackWW <zvobabic@hotmail.com>
2022-12-20 08:58:21 +0100

    Add: assets

---
+++ lib/constants/assets.dart
@@ -0,0 +1,7 @@
+class ImageAssets {
+  static const towerDay = 'assets/images/tower_day.png';
+  static const towerNight = 'assets/images/tower_night.png';
+  static const chicken = 'assets/images/chicken.jpg';
+  static const fresh = 'assets/images/fresh.jpg';
+  static const hippo = 'assets/images/hippo.jpg';
+}
