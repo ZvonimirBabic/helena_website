e83a7ea368bca4782efaa0c90c14e009d90d053f
BlackWW <zvobabic@hotmail.com>
2022-12-20 10:20:33 +0100

    Add: Text styles, themes, colors, localization WIP

---
+++ lib/constants/colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+class CustomColors {
+  /// Default colors
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+
+  /// Primary colors
+  static const Color primaryBlue = Color(0xFF00ADAD);
+  static const Color primaryLightBlue = Color(0xFF00E0CD);
+  static const Color primaryDarkBlue = Color(0xFF32496B);
+
+  static const Color primaryRed = Color(0xFFD62839);
+
+  static const Color selectedBlue = Color(0xFF32496B);
+}
---
+++ lib/constants/themes.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+class Themes {
+  /// Light theme
+  static final lightTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(color: CustomColors.primaryBlue),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        backgroundColor: CustomColors.white.withOpacity(0.2),
+        selectedLabelStyle: const TextStyle(color: Colors.white),
+        selectedItemColor: CustomColors.white,
+        unselectedLabelStyle: const TextStyle(color: Colors.white),
+        unselectedItemColor: Colors.white),
+    selectedRowColor: Colors.white,
+  );
+
+  /// Dark theme
+  static final darkTheme = ThemeData();
+}
